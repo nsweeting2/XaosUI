ac6496e49a4144c8d598ac139c18477ba7b63209
nsweeting2 <55107062+nsweeting2@users.noreply.github.com>
2019-09-10 19:18:33 -0400

    added base xPrompt

---
+++ xPrompt.lua
@@ -0,0 +1,16 @@
+return {{
+  [ [[hp]] ] = [[50]],
+  [ [[mana]] ] = [[50]],
+  [ [[move]] ] = [[50]],
+  [ [[exp]] ] = [[25]],
+  [ [[exits]] ] = [[NSEWUD]],
+  [ [[style]] ] = [[Normal]],
+  [ [[hunger]] ] = [[F]],
+  [ [[hungerWords]] ] = [[Fine]],
+  [ [[race]] ] = [[Human]],
+  [ [[room]] ] = [[Crossroads]],
+  [ [[time]] ] = [[Afternoon]],
+  [ [[vnum]] ] = [[1000]],
+  [ [[area]] ] = [[Visari]],
+  [ [[tell]] ] = [[Noax tells you 'Idle check!']]
+}}
