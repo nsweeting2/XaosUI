a1504c69ecbd00d5b88f79edaa4094bdda5b8b36
nsweeting2 <55107062+nsweeting2@users.noreply.github.com>
2019-09-13 10:36:31 -0400

    .

--- scripter.lua
+++ scripter.lua
@@ -5,7 +5,7 @@ return {{
   [ [[Area]] ] = [[none]],
   [ [[Rate]] ] = [[4]],
   [ [[Threshold]] ] = [[85]],
-  [ [[Wimpy]] ] = [[50]]
+  [ [[Wimpy]] ] = [[50]],
   [ [[Bugout]] ] = [[40]],
   [ [[EmergencyTrigger]] ] = [[60]],
   [ [[EmergencyAction]] ] = [[flee]],
