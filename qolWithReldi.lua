ded3db159fdd20b6aad1fdc495e1a3378ba6872e
Reldi <lautensack@gmail.com>
2020-03-09 20:27:32 -0700

    Revert "XaosUI 2.0.0.8 - Big Scripter Update + Few UI Fixes"
    This reverts commit de1f6101a5062bd7d54160c5a0be7f04dbb95110.

--- qolWithReldi.lua
+++
@@ -1,9 +0,0 @@
-return {{
-  [ [[Resolving]] ] = [[false]],
-  [ [[Trancing]] ] = [[false]],
-  [ [[Berserking]] ] = [[false]],
-  [ [[Warcrying]] ] = [[false]],
-  [ [[Warcry]] ] = [[off]],
-  [ [[Centering]] ] = [[false]],
-  [ [[HealWord]] ] = [[heal]],
-},}
