d059c056d60db66db2f3952bb0e7e82284ab2958
Reldi <lautensack@gmail.com>
2020-03-09 22:09:40 -0700

    Simple Push to get qolWithReldi Up

---
+++ qolWithReldi.lua
@@ -0,0 +1,9 @@
+return {{
+  [ [[Resolving]] ] = [[false]],
+  [ [[Trancing]] ] = [[false]],
+  [ [[Berserking]] ] = [[false]],
+  [ [[Warcrying]] ] = [[false]],
+  [ [[Warcry]] ] = [[off]],
+  [ [[Centering]] ] = [[false]],
+  [ [[HealWord]] ] = [[heal]],
+},}
